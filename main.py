98029049af1b5d33f5e3dff270b3c1dacbb19559
kairat <63288004+Kairat77@users.noreply.github.com>
2024-03-22 14:11:39 +0600

    add model

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from aiogram import Bot, Dispatcher
-from config import TOKEN
 import asyncio
-
-
-
-
-
+from config import TOKEN
+from aiogram import Bot, Dispatcher
+from app.database.models import async_main
 
 async def main():
+  await async_main()
   bot = Bot(token=TOKEN)
-  dp = Dispatcher(bot=bot)
+  dp = Dispatcher()
   await dp.start_polling(bot)
 
 if __name__ == '__main__':
-  asyncio.run(main())
+  try:
+    asyncio.run(main())
+  except KeyboardInterrupt:
+    print("Stopped by Ctrl+C")
