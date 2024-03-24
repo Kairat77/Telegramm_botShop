3f80e24e8c2b6e081dbc75a1b96366fba4a76325
kairat <63288004+Kairat77@users.noreply.github.com>
2024-03-24 12:13:30 +0600

    add requests, handlers, keyboards

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import BigInteger, ForeignKey, String
 from sqlalchemy.orm import relationship, mapped_column, DeclarativeBase, Mapped
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
 from config import ENGINE, ECHO
 from typing import List
 
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import asyncio
 from config import TOKEN
 from aiogram import Bot, Dispatcher
 from app.database.models import async_main
+from app.handlers import router
+import logging
 
 async def main():
   await async_main()
   bot = Bot(token=TOKEN)
   dp = Dispatcher()
+  dp.include_router(router)
   await dp.start_polling(bot)
 
 if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
   try:
     asyncio.run(main())
   except KeyboardInterrupt:
